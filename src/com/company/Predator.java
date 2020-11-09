b30dfafb3cf19574ba156527186082cdfd16fe79
Théodule <theodule.janchen@edu.itescia.fr>
2020-11-09 15:00:00 +0100

    beginning of prey and predator implementations : variable & method declaration without any code yet

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -1,4 +1,35 @@
 package com.company;
 
 public class Predator {
+
+    int nbLifePoints;
+    int nbLifePointsMax;
+    static final int nbLifePointsMaxAll = 100;
+
+    int age;
+    int maxAge;
+    static final int maxAgeAll = 42;
+
+    Smelling smelling;
+    Vision vision;
+
+    public void hunt(){
+
+    }
+
+    public void mate(){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void eat(){
+
+    }
+
+    public void drink(){
+
+    }
 }
--- src/com/company/Prey.java
+++ src/com/company/Prey.java
@@ -1,4 +1,35 @@
 package com.company;
 
 public class Prey {
+
+    int nbLifePoints;
+    int nbLifePointsMax;
+    static final int nbLifePointsMaxAll = 100;
+
+    int age;
+    int maxAge;
+    static final int maxAgeAll = 42;
+
+    Hearing hearing;
+    Vision vision;
+
+    public void runAway(){
+
+    }
+
+    public void mate(){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void eat(){
+
+    }
+
+    public void drink(){
+
+    }
 }
